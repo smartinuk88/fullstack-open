46a5e9bdcabd844c0a30310b2702f747e067fc6b
Scott Martin <scott.martin88@live.com>
2026-04-23 07:42:34 +0200

    Complete 3.3: Create route for individual person

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,17 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const { id } = request.params;
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 app.get("/info", (request, response) => {
   const info = `<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`;
   response.send(info);
