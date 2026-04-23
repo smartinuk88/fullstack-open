dfa705e2aa716ef2e9f4782277a466a945513794
Scott Martin <scott.martin88@live.com>
2026-04-23 06:58:32 +0200

    Create backend functions in part3 sandbox project

--- part3/hello-world/index.js
+++ part3/hello-world/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let notes = [
   {
     id: "1",
@@ -18,6 +20,12 @@ let notes = [
     important: true,
   },
 ];
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((note) => Number(note.id))) : 0;
+  return String(maxId + 1);
+};
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -43,6 +51,23 @@ app.delete("/api/notes/:id", (request, response) => {
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+  };
+
+  notes = notes.concat(note);
+  response.json(note);
 });
 
 const PORT = 3001;
