043728d3609a0d5813419aadc0c7711226c7d957
Scott Martin <scott.martin88@live.com>
2026-04-24 16:18:53 +0200

    Complete 3.5 and 3.6: Implement func for adding new persons

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -23,6 +24,8 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const generateId = () => String(Math.floor(Math.random() * 1000000));
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
@@ -49,6 +52,29 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  const existingPerson = persons.find(
+    (person) => person.name.toLowerCase() === body.name?.toLowerCase(),
+  );
+
+  if (!body.name || !body.number || existingPerson) {
+    return response
+      .status(400)
+      .json({ error: "Name or number missing, or person already added" });
+  }
+
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+  response.json(newPerson);
 });
 
 const PORT = 3001;
